d073211ebeaf26043c49c9e2f0e7e6edea92f7a5
Meztake <gameholin123@mail.ru>
2020-07-16 21:13:21 +0300

    added find

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task("sass", function () {
             suffix: ".min"
         }))
         .pipe(autoprefixer({
-            overrideBrowserslist: ["last 8 versions"]
+            overrideBrowserslist: ["last 8 versions"],
         }))
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.reload({
@@ -28,7 +28,8 @@ gulp.task("style", function () {
     return gulp.src([
             "node_modules/normalize.css/normalize.css",
             "node_modules/slick-carousel/slick/slick.css",
-            "node_modules/magnific-popup/dist/magnific-popup.css"
+            "node_modules/magnific-popup/dist/magnific-popup.css",
+            "node_modules/rateyo/src/jquery.rateyo.css"
         ])
 
         .pipe(concat("libs.min.css"))
@@ -39,7 +40,9 @@ gulp.task("style", function () {
 gulp.task("script", function () {
     return gulp.src([
             "node_modules/slick-carousel/slick/slick.js",
-            "node_modules/magnific-popup/dist/jquery.magnific-popup.js"
+            "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+            "node_modules/mixitup/dist/mixitup.js",
+            "node_modules/rateyo/src/jquery.rateyo.js"
         ])
 
         .pipe(concat("libs.min.js"))
@@ -48,9 +51,9 @@ gulp.task("script", function () {
 });
 
 gulp.task("html", function () {
-    return gulp.src("app/*html").pipe(browserSync.reload({
+    return gulp.src("app/*.html").pipe(browserSync.reload({
         stream: true
-    }));
+    }))
 });
 
 gulp.task("js", function () {
@@ -62,15 +65,15 @@ gulp.task("js", function () {
 gulp.task("browser-sync", function () {
     browserSync.init({
         server: {
-            baseDir: "app/",
-        },
+            baseDir: "app/"
+        }
     });
 });
 
 gulp.task("watch", function () {
-    gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
-    gulp.watch("app/*.html", gulp.parallel("html"));
-    gulp.watch("app/js/*.js", gulp.parallel("js"));
+    gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"))
+    gulp.watch("app/*.html", gulp.parallel("html"))
+    gulp.watch("app/js/*.js", gulp.parallel("js"))
 });
 
 gulp.task("default", gulp.parallel("style", "script", "sass", "watch", "browser-sync"))
